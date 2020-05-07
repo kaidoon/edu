4f12c721a7178d64795667c81d291063ce9d8f6a
kaidoon <richstoe@gmail.com>
2020-05-08 00:12:18 +0200

    Add initial version

--- fluent_python_01.py
+++ fluent_python_01.py
@@ -1,7 +1,25 @@
 import pprint as pp
+import array
 
 symbols = '¢æ…–¢¬¢¶äööü¬¢ääöä'
 # the symbol var in the listcomp (list comprehention) are
-codes = [ord(symbol) for symbol in symbols]
-pp.pprint(codes)
+codes_listcom = [ord(symbol) for symbol in symbols]
+pp.pprint('codes_listcom :: ')
+pp.pprint(codes_listcom)
 
+# generator expressions : genexp
+codes_genexp = tuple(ord(symbol) for symbol in symbols)
+pp.pprint('codes_genexp :: ')
+pp.pprint(codes_genexp)
+
+# cartesian products
+colors = ['yellow', 'red', 'blue', 'black']
+sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+tshirts = [(color, size) for color in colors for size in sizes]
+pp.pprint('codes_listcom (cartesian product) :: ')
+pp.pprint(tshirts)
+
+# array
+codes_array = array.array('I', (ord(symbol) for symbol in symbols))
+pp.pprint('codes_array :: ')
+pp.pprint(codes_array)
