87aa9d41c41923fd0550443c67b48f927e706c77
kaidoon <richstoe@gmail.com>
2020-05-13 22:08:07 +0200

    Add theme slices handling

--- fluent_python_01.py
+++ fluent_python_01.py
@@ -139,3 +139,23 @@ s = 'bicycle'
 print(s[::3])
 print(s[::-1])
 print(s[::-2])
+
+item = '1909  Pimoroni PrBrella                 $17.50'
+SKU = slice(0, 6)
+DESCR = slice(6, 40)
+UNIT_PRICE = slice(40, 52)
+print(item[SKU])
+print(item[DESCR])
+print(item[UNIT_PRICE])
+
+#Assigning to slices
+l = list(range(10))
+print(l)
+l[2:5] = [20, 30]
+print(l)
+del l[5:7]
+print(l)
+l[3::2] = [11, 22]
+print(l)
+l[2:5] = [100]
+print(l)
